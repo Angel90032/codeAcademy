15727a041635f6ec310a38f13af7faaeca6d0637
Angel90032 <52039209+Angel90032@users.noreply.github.com>
2021-01-16 08:15:29 +0200

    Update 20210115_Task15.c

--- 20210115/20210115_Task15.c
+++ 20210115/20210115_Task15.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-void test();
-static int g_someNumber;
+void test(){
+    static int g_someNumber = 1000;
+    g_someNumber++;
+    printf("%i\n",g_someNumber);
+}
+
 int main(void){
     test();
     test();
     test();
-    printf("%i",g_someNumber);
+    
 
 }
 
-static int g_someNumber = 100;
 
-void test(){
-    g_someNumber++;
-}
+
