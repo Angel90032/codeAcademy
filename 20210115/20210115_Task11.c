5334cf9f2ee92cfecf8387e4df5b282e7ff9ac62
Angel Gatev <angelgatev@abv.bg>
2021-01-20 08:50:11 +0200

    g

--- 20210115/20210115_Task11.c
+++ 20210115/20210115_Task11.c
@@ -9,11 +9,16 @@ int main(void){
     char cPos3 = str[3];
     char cPos4 = str[4];
     
-    char rts[] = {};
+    char rts[20] = "";
+    printf("%s\n",rts);
     rts[0] = cPos4;
+    printf("%s\n",rts);
     rts[1] = cPos3;
+    printf("%s\n",rts);
     rts[2] = cPos2;
+    printf("%s\n",rts);
     rts[3] = cPos1;
+    printf("%s\n",rts);
     rts[4] = cPos0;
     printf("%s\n",rts);
 
