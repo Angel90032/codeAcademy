799dcd0d52a7640298c35f29bc9ab677784586c8
Angel90032 <52039209+Angel90032@users.noreply.github.com>
2021-01-22 18:12:56 +0200

    Using index++ and for cycle

---
+++ TEAM_8TASK13/Task13Alternative2.c
@@ -0,0 +1,57 @@
+
+/*Използвайте do-while цикъл, за да напишете функцията void itoa(int n, char s[]), 
+която преобразува число в символен низ (обратната функция на atoi). 
+do-while цикълът e необходим, тъй като в масива с ще трябва да се постави поне един символ, дори ако n е нула.
+ Ако n = 123, за да вземем последната цифра, трябва да разделим n по модул(%) на основата на бройната система 10. 
+ За да получите чаръктър число, трябва да съберете резултата с '0'. 
+ За да вземете следващото число, трябва да разделите n на 10. 
+ Това продължава докато n е различно от нула. Това генерира низа в обратен ред – 321, затова обърнете низа */
+
+#include <stdio.h>
+#include <string.h>
+
+void itoa(int iSomeInt, char someString[]){
+    int index = 0;
+    do{
+        char lastDigitAsChar;
+        printf("We have added %c to our string\n",lastDigitAsChar);
+        printf("Our current iSomeInt is %i\n",iSomeInt);
+        printf("Our current someString is: %s\n",someString);
+        printf("-------------------------------------------------------\n");
+
+        int lastDigit = iSomeInt % 10;
+        iSomeInt = iSomeInt / 10;
+        lastDigitAsChar = (char)(lastDigit) + '0';
+        char stringToAdd[10000];
+        someString[index] = lastDigitAsChar;
+        //strncat(someString,stringToAdd,1);
+        index++;
+
+        printf("We have added %c to our string\n",lastDigitAsChar);
+        printf("Our current number is %i\n",iSomeInt);
+        printf("Our current string is: %s\n",someString);
+        printf("-------------------------------------------------------\n");
+
+
+    }while( iSomeInt != 0);
+
+    printf("The cycle has ended.\n");
+    //someString = strrev(someString);
+    int x = strlen(someString) -1;
+    int newIndex = 0;
+    char finalString[100];
+    for (x; x >=0; x-- ){
+        finalString[newIndex] = someString[x];
+        newIndex++;
+    }
+    printf("Our correct string is %s",finalString);
+
+}
+
+int main(void){
+    int iMyNumber;
+    char cMystring[10000] = {};
+    printf("Enter your number:\n");
+    scanf("%i",&iMyNumber);
+    itoa(iMyNumber,cMystring);
+}
