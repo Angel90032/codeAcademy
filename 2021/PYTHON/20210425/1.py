ed14e4dc91b9513b269405a0aa491909b4fd565a
Angel90032 <52039209+Angel90032@users.noreply.github.com>
2021-04-27 11:32:08 +0300

    Update 1.py

--- 2021/PYTHON/20210425/1.py
+++ 2021/PYTHON/20210425/1.py
@@ -1,1 +1,24 @@
 
+#overloading
+
+def my_adder(a,b):
+    
+    if (type(a) is int) and (type(b) is int):
+        c = a + b
+        
+    
+    elif (type(a) is str) and (type(b) is str ):
+        c = a + " " + b
+        
+    
+    elif (type(a) is int) and (type(b) is str):
+        
+        c = a * b
+        
+    return c
+    
+
+
+print(my_adder(10, 5))
+print(my_adder("Hello","there"))
+print(my_adder(3,"GO!"))
